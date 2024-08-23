c752b60afe273d132f2000c2f37c2fa0ebc10607
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2024-08-23 08:43:14 -0300

    asd

--- exercicio-00/main.js
+++ exercicio-00/main.js
@@ -1,10 +1,22 @@
 // Criando um contador de cliques em um botão usando JavaScript para manipular o DOM.
 
-const btn = document.querySelector('button');
-const count = document.querySelector('h3');
+const btn = document.querySelector('button'); // Selecionando o botão
+const count = document.querySelector('h3'); // Selecionando o contador
 
-let counter = 0;
+// Adicionando um evento de clique no botão
 btn.addEventListener('click', () => {
-    counter += 1;
+    // Pegando o valor do contador no localStorage
+    const counter = localStorage.getItem('counter') || 0;
+    // Convertendo o valor do contador para inteiro e incrementando 1
+    const newCounter = parseInt(counter) + 1;
+    
+    // Atualizando o contador
+    localStorage.setItem('counter', newCounter);
+    count.textContent = newCounter;
+});
+
+// Adicionando um evento de carregamento da página
+window.addEventListener('load', () => {
+    const counter = localStorage.getItem('counter') || 0;
     count.textContent = counter;
 });
